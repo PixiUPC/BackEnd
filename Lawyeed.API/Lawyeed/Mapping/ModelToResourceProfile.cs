8a6fd63b9aa3e5fa56e69a4cf33f2854e75137c2
djalmadioses <65045652+maucaillesamuel02@users.noreply.github.com>
2022-09-14 09:51:16 -0500

    feat: add person-plan model and resource

--- Lawyeed.API/Lawyeed/Mapping/ModelToResourceProfile.cs
+++ Lawyeed.API/Lawyeed/Mapping/ModelToResourceProfile.cs
@@ -12,6 +12,7 @@ public class ModelToResourceProfile : Profile
         CreateMap<Person, PersonResource>();
         CreateMap<PersonLawyer, PersonLawyerResource>();
         CreateMap<Plan, PlanResource>();
+        CreateMap<PersonPlan, PersonPlanResource>();
 
     }
 }
---
+++ Lawyeed.API/Lawyeed/Resources/PersonPlanResource.cs
@@ -0,0 +1,9 @@
+namespace Lawyeed.API.Lawyeed.Resources;
+
+public class PersonPlanResource
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string PersonId { get; set; }
+    public string PlanId { get; set; }
+}
